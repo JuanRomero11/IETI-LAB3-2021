c7bccd5474d60f6be1fef2895cbb4109524a5ec6
JuanRomero11 <juan.romero-si@mail.escuelaing.edu.co>
2021-02-25 00:27:06 -0500

    readme

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 ReactDOM.render(
@@ -10,3 +11,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+// serviceWorker.unregister();
